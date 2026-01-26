22045c2e23d5d8c3e3a4b56f693e63639082bcec
Ben Wang <benwangott@gmail.com>
2026-01-25 20:37:23 -0500

    pyaudio playback

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-#!./bin/python3
-import time
+# import time
 from ytmusicapi import YTMusic
 import yt_dlp
-import vlc
+# import vlc
 import sys
 import glob
+import wave
+import pyaudio
 
 # replacing hard coded strings with variables
-video_url = 'youtu.be/'
+video_url = 'https://youtu.be/'
 
-ydl_opts = {}
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+    'js_runtimes': {'node':{}, 'deno':{}} # FIXME
+}
+
+chunk = 1024
 
 ytmusic = YTMusic()
 # video_url += ytmusic.search("Bohemian Rhapsody")[0]['videoId']
@@ -21,6 +32,21 @@ if len(sys.argv) >= 2:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
 
-    p = vlc.MediaPlayer(glob.glob(f'*[[]{video_id}[]].webm')[0])
-    p.play()
-    time.sleep(int(ytmusic.search(sys.argv[1])[0]['duration_seconds']))
+    # p = vlc.MediaPlayer(glob.glob(f'*[[]{video_id}[]].mp3')[0])
+    # p.play()
+    # time.sleep(int(ytmusic.search(sys.argv[1])[0]['duration_seconds']))
+
+    wf = wave.open(glob.glob(f'*[[]{video_id}[]].wav')[0])
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+    data = wf.readframes(chunk)
+    while data:
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
